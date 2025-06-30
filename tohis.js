5df2e771310ce1450f99baf86795198012e29315
憂鬱のb（￣▽￣）d <117074497+SoftOSDev@users.noreply.github.com>
2025-06-30 18:06:12 +0800

    Fixing Bugs

--- tohis.js
+++ tohis.js
@@ -1,1 +1,16 @@
-var xhr=new XMLHttpRequest();xhr.open('GET','https://v2.xxapi.cn/api/history',false);xhr.send();var aa=xhr.responseText;var result=JSON.parse(aa);console.log(result);const jsonObj=JSON.parse(aa);while(true){num=Math.floor(Math.random()*(20-0+1))+0; data=jsonObj['data'][num.toString()];if(data!=undefined)break}console.log(data);document.getElementById("tohis").innerHTML=data;
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://v2.xxapi.cn/api/history', false); 
+xhr.send();
+
+    var aa = xhr.responseText;
+    var result = JSON.parse(aa);
+    
+    console.log(result);
+const jsonObj = JSON.parse(aa);
+while(true){
+    num=Math.floor(Math.random() * (20 - 0 + 1)) + 0
+    data = jsonObj['data'][num.toString()];
+    if(data!=undefined)break;
+}
+console.log(data); 
+document.getElementById("tohis").innerHTML=data;
